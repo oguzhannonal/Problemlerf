784d7da53d03315205111c9c927bff2e54ab9cd6
sheldorion <oguzhanonal52@hotmail.com>
2018-10-27 02:50:15 +0300

    Create dizisiralama.c

---
+++ dizisiralama.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define BOYUT 10
+
+int main(void){
+
+
+int d;//dizinin içinde dolasma
+int k;//verilerin birbiriyle karşılaştırması
+int gecici;//gecici olarak verileri tutmak için
+
+int dizi[BOYUT]={1,3,5,17,4,19,10,100,2,198};
+for(d=0;d<BOYUT;d++){
+
+    printf("%d ",dizi[d]);
+
+}
+
+for(d=0;d<BOYUT;d++){
+
+    for(k=0;k<BOYUT-1;k++){
+        if(dizi[k]>dizi[k+1]){
+            gecici=dizi[k];
+            dizi[k]=dizi[k+1];
+            dizi[k+1]=gecici;
+        }
+    }
+
+
+}
+for(d=0;d<BOYUT;d++){
+
+    printf("%d ",dizi[d]);
+}
+return 0;
+}
